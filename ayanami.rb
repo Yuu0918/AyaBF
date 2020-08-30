5a40afe2b6160701deecd987504f31b91ae3d244
Yuu0918 <yuma200416@gmail.com>
2020-08-31 00:19:20 +0900

    初版

---
+++ ayanami.rb
@@ -0,0 +1,15 @@
+Encoding.default_external = "UTF-8"
+require 'r-fxxk'
+
+class Aya < Brainfuck
+  nxt "芋っぽくて"
+  inc "綾波"
+  prv "鬼神で"
+  dec "かわいい"
+  opn "おっとりしていて"
+  cls "純粋無垢で"
+  put "は"
+  get "敷波"
+end
+
+puts Aya.new.fuck(ARGF.read)
